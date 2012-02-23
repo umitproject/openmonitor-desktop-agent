3ac4bd248dbe44b064f852a608315bb82c2afda5
Adriano Marques <adriano@umitproject.org>
2012-02-23 23:20:28 +0800

    removed the line for test

--- umit/icm/agent/config.py
+++ umit/icm/agent/config.py
@@ -18,8 +18,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-import importer
-
 import os
 
 from ConfigParser import ConfigParser, DEFAULTSECT, NoOptionError, \
